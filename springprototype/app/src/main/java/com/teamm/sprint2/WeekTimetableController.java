b9611166ffe5f62e03c27b9b8f4bb082b7db4c2d
Sean Jakobsen <skwjakobsen@gmail.com>
2013-11-30 21:34:55 +0000

    Foundation for Timetable controllers

---
+++ springprototype/app/src/main/java/com/teamm/sprint2/WeekTimetableController.java
@@ -0,0 +1,41 @@
+package com.teamm.sprint2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class WeekTimetableController {
+	private String[] courses = {"Course1", "Course2", "Course3"};
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/timetable/week/", method = RequestMethod.GET)
+	public String timetable(Locale locale, Model model) {
+		logger.info("Viewing timetable. The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("courses", courses);
+		
+		return "timetable/week";
+	}
+	
+}
